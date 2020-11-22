85d1579440a29405fe852947a000b293746335e9
mfi7000 <44906375+mfi7000@users.noreply.github.com>
2020-11-22 20:52:37 +0100

    Stylet integrated

---
+++ Mfi.Wpf.Aquarius.TestApplication/Startup/AppBootstrapper.cs
@@ -0,0 +1,9 @@
+﻿using Mfi.Wpf.Aquarius.TestApplication.ViewModels;
+
+namespace Mfi.Wpf.Aquarius.TestApplication.Startup
+{
+    public class AppBootstrapper : Bootstrappers.AutofacBootstrapper<MainWindowViewModel>
+    {
+        
+    }
+}
---
+++ Mfi.Wpf.Aquarius.TestApplication/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mfi.Wpf.Aquarius.TestApplication.ViewModels
+{
+    public class MainWindowViewModel : Stylet.PropertyChangedBase   
+    {
+    }
+}
