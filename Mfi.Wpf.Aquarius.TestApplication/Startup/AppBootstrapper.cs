7b24fe3a5d3909fd1cdcb323020868b8fce58b6d
mfi7000 <44906375+mfi7000@users.noreply.github.com>
2020-11-23 19:25:01 +0100

    Bootstrapper moved to Lib

--- Mfi.Wpf.Aquarius.TestApplication/Startup/AppBootstrapper.cs
+++ Mfi.Wpf.Aquarius.TestApplication/Startup/AppBootstrapper.cs
@@ -1,9 +1,15 @@
-﻿using Mfi.Wpf.Aquarius.TestApplication.ViewModels;
+﻿using DevExpress.Xpf.Core;
+using Mfi.Wpf.Aquarius.StyletMods.Bootstrappers;
+using Mfi.Wpf.Aquarius.TestApplication.ViewModels;
 
 namespace Mfi.Wpf.Aquarius.TestApplication.Startup
 {
-    public class AppBootstrapper : Bootstrappers.AutofacBootstrapper<MainWindowViewModel>
+    public class AppBootstrapper : AutofacBootstrapper<MainWindowViewModel>
     {
-        
+        protected override void OnStart()
+        {
+            base.OnStart();
+            ApplicationThemeHelper.ApplicationThemeName = Theme.VS2019DarkName;
+        }
     }
 }
--- Mfi.Wpf.Aquarius.TestApplication/Views/MainWindowView.xaml.cs
+++ Mfi.Wpf.Aquarius.TestApplication/Views/MainWindowView.xaml.cs
@@ -1,13 +1,14 @@
 ﻿using System.Windows;
+using DevExpress.Xpf.Core;
 
 namespace Mfi.Wpf.Aquarius.TestApplication.Views
 {
     /// <summary>
     /// Interaction logic for MainWindowView.xaml
     /// </summary>
-    public partial class MainWindowView : Window
+    public partial class MainWindowView : DXWindow
     {
-        public MainWindowView()
+        public MainWindowView() : base()
         {
             InitializeComponent();
         }
